be1d41176e3a722151c685c11024c1ce3d6fa881
ACEDIA2567 <sdman2567@gmail.com>
2024-06-20 16:47:34 +0900

    Feat: Resources를 이용하여 연결

---
+++ Assets/Scripts/Entity/AudioClips.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum SFXClip
+{
+    Attack,
+    Die
+}
+
+public class AudioClips : MonoBehaviour
+{
+    public AudioClip[] clips;
+}
--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -1,24 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundManager : Singleton<SoundManager>
 {
     public AudioMixer mixer;
 
-    public AudioClip[] clips;
-    public AudioSource AudioSource;
+    public AudioClip[] clipSFX;
+    public AudioClip[] clipBGM;
+    public AudioSource sourceMaster;
+    private AudioSource sourceSFX;
+    private AudioSource sourceBGM;
+
+    private void Awake()
+    {
+        AudioInit();
+    }
+
+    private void AudioInit()
+    {
+        GameObject bgm = Instantiate(Resources.Load<GameObject>("Sound/BGMSource"), transform);
+        GameObject sfx = Instantiate(Resources.Load<GameObject>("Sound/SFXSource"), transform);
+        clipBGM = bgm.GetComponent<AudioClips>().clips;
+        clipSFX = bgm.GetComponent<AudioClips>().clips;
+        sourceBGM = bgm.GetComponent<AudioSource>();
+        sourceSFX = sfx.GetComponent<AudioSource>();
+    }
 
     public void SetMasterVolum(float sliderValue)
     {
         mixer.SetFloat("MusicVol" ,Mathf.Log10(sliderValue) * 20);
     }
 
+    public void MasterVolumMut(Toggle toggle)
+    {
+        sourceMaster.mute = toggle;
+    }
+
+    public void BGMVolumMut(Toggle toggle)
+    {
+        sourceSFX.mute = toggle;
+    }
+
+    public void SFXVolumMut(Toggle toggle)
+    {
+        sourceBGM.mute = toggle;
+    }
+
     public void SetBGMVolume(float sliderValue)
     {
         mixer.SetFloat("BGM", Mathf.Log10(sliderValue) * 20);
     }
+
     public void SetSFXVolume(float sliderValue)
     {
         mixer.SetFloat("SFX", Mathf.Log10(sliderValue) * 20);
@@ -26,12 +62,26 @@ public class SoundManager : Singleton<SoundManager>
 
     public void PlaySound(string name)
     {
-        for (int i = 0; i < clips.Length; i++)
+        for (int i = 0; i < clipBGM.Length; i++)
         {
-            if (name == clips[i].name)
+            if (name == clipBGM[i].name)
             {
-                AudioSource.clip = clips[i];
-                AudioSource.Play();
+                sourceBGM.Stop();
+                sourceBGM.clip = clipBGM[i];
+                sourceBGM.Play();
+            }
+        }
+    }
+
+
+
+    public void PlayShot(string name)
+    {
+        for (int i = 0; i < clipSFX.Length; i++)
+        {
+            if (name == clipSFX[i].name)
+            {
+                sourceSFX.PlayOneShot(clipSFX[i]);
             }
         }
     }
